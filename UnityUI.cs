a195235b0d6e08268308f7e77c281ed68712e725
George Markosian <georgemarkosian@gmail.com>
2022-11-02 23:01:10 +0100

    Tablet detection improvement. ScreenDPI public property added.

--- UnityUI.cs
+++ UnityUI.cs
@@ -10,6 +10,7 @@ namespace Build1.UnityUI
         public static ScreenOrientation DeviceOrientation    => _agent.DeviceOrientation;
         public static int               ScreenWidth          => _agent.ScreenWidth;
         public static int               ScreenHeight         => _agent.ScreenHeight;
+        public static float             ScreenDPI            => Screen.dpi;
 
         public static event Action<InterfaceType> OnInterfaceTypeChanged;
         public static event Action                OnDeviceOrientationChanged;
@@ -21,16 +22,15 @@ namespace Build1.UnityUI
         static UnityUI()
         {
             #if UNITY_EDITOR
-            
+
             _agent = InitializeAgent(UnityUIAgentEditor.Create());
-            
+
             #else
-            
             var root = new GameObject("[UnityUI]");
             UnityEngine.Object.DontDestroyOnLoad(root);    
             
             _agent = InitializeAgent(UnityUIAgentRuntime.Create(root));
-            
+
             #endif
 
             CurrentInterfaceType = _agent.GetInterfaceType();
@@ -39,7 +39,7 @@ namespace Build1.UnityUI
         /*
          * Public.
          */
-        
+
         public static float GetScreenAspectRatio()
         {
             return Mathf.Max((float)_agent.ScreenWidth, _agent.ScreenHeight) / Mathf.Min(_agent.ScreenWidth, _agent.ScreenHeight);
@@ -54,11 +54,8 @@ namespace Build1.UnityUI
 
         internal static bool IsTablet()
         {
-            var diagonal = GetScreenDiagonalInches();
-            var aspect = GetScreenAspectRatio();
-            if (aspect >= 1.77)
-                return diagonal >= 7F;
-            return diagonal >= 5F;
+            return GetScreenDiagonalInches() >= 7F && 
+                   GetScreenAspectRatio() <= 1.77;
         }
 
         /*
